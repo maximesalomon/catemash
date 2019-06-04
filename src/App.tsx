61344e8ff40e6ecfe92ffa3f2ac7bd6fea3cc07c
Maxime Salomon <maxime@croissant.io>
2019-06-04 19:12:50 +0200

    Add grapql query for cats

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const App = () => {
+import { API, graphqlOperation } from 'aws-amplify'
+
+const query = `
+  query {
+    listCats {
+      items {
+        id url
+      }
+    }
+  }
+`
+
+interface ICat {
+  id: string;
+  url: string;
+}
+
+const App = () => { 
+  const [cats, setCats] = useState([])
+
+  async function fetchGraphqlAPI() {
+    const data: any = await API.graphql(graphqlOperation(query))
+    setCats(data.data.listCats.items)
+  }
+
+  useEffect(() => {
+    fetchGraphqlAPI();
+  }, []);
+
+  console.log(cats)
+
   return (
     <div className="App">
-        <h1>Catmash</h1>
-      </div>
+      <h1>Catmash</h1>
+    </div>
   )
 }
 
