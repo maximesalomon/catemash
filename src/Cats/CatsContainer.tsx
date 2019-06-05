aba56122f340ac5f2c8835d72a69fcfaeb0a78cd
Maxime Salomon <maxime@croissant.io>
2019-06-05 21:02:36 +0200

    wip graphQL problems
    I have been struggling to get 2 randoms id from graphQL. Will look at it tomorrow

--- src/Cats/CatsContainer.tsx
+++ src/Cats/CatsContainer.tsx
@@ -2,9 +2,23 @@ import React, { useState, useEffect } from "react";
 import { API, graphqlOperation } from "aws-amplify";
 import styled from 'styled-components'
 
-import Cats from './Cats'
+import Cats from './Cats';
 
-const query = `
+const queryIds = `
+  query {
+    listCats(limit: 101) {
+      items {
+        ...catFields
+      }
+    }
+  }
+
+  fragment catFields on Cat {
+    id
+  }
+`;
+
+const queryCats = `
   query {
     listCats(limit: 2) {
       items {
@@ -20,7 +34,6 @@ const query = `
   }
 `;
 
-
 interface ICat {
   id: string;
   url: string;
@@ -28,22 +41,47 @@ interface ICat {
 }
 
 const CatsContainer = () => {
+    const [started, setStarted] = useState(false);
+    const [ids, setIds] = useState([]);
+    const [id1, setId1] = useState('');
+    const [id2, setId2] = useState('');
     const [cats, setCats] = useState([]);
 
-    async function fetchGraphqlAPI() {
-        const data: any = await API.graphql(graphqlOperation(query));
-        setCats(data.data.listCats.items);
+    const randomIds = () => {
+      let randomId1 = ids[Math.floor(Math.random() * ids.length)];
+      let randomId2 = ids[Math.floor(Math.random() * ids.length)];
     }
 
-    useEffect(() => {
-        fetchGraphqlAPI();
-    }, []);
-    return (
-      <CatsContainerStyle>
+    const fetchData = async () => {
+      const ids: any = await API.graphql(graphqlOperation(queryIds));
+      setIds(ids.data.listCats.items);
+      if (ids.length = 101) {
+        let randomId1 = ids[Math.floor(Math.random() * ids.length)];
+        let randomId2 = ids[Math.floor(Math.random() * ids.length)];
+        setId1(randomId1);
+        console.log(id1)
+        const data: any = await API.graphql(graphqlOperation(queryCats));
+        setCats(data.data.listCats.items);
+        return null
+      } else {
+        return alert("Refresh page to fetch cats")    
+      }
+    }
+
+    const handleClick = () => {
+      setStarted(true);
+      fetchData();
+    }
+
+    const start = started === false
+      ? <button onClick={() => handleClick()}>Start</button>
+      : <CatsContainerStyle>
           {cats.map((cat: ICat) => (
-              <Cats key={cat.id} cat={cat} />
-          ))}
-      </CatsContainerStyle>
+                <Cats key={cat.id} cat={cat} />
+            ))}
+        </CatsContainerStyle>
+    return (
+      start
     )
 }
 
