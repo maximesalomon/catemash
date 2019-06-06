23dc382a753b94676b7cc7bf7dd533bf7b1a4899
Maxime Salomon <maxime@croissant.io>
2019-06-06 22:49:30 +0200

    Add wanted GraphQL request
    I have spent 2 hours yesterday and 2 more today trying to understad how GraphQL works. My conclusion is that I need to create a resolver getRamdom with a limit parameter of 2. However, I am not able to do so in a timely fashion with my current knowledge of GraphQL.

--- src/Cats/CatsContainer.tsx
+++ src/Cats/CatsContainer.tsx
@@ -4,26 +4,10 @@ import styled from 'styled-components'
 
 import Cats from './Cats';
 
-const queryIds = `
-  query {
-    listCats(limit: 101) {
-      items {
-        ...catFields
-      }
-    }
-  }
-
-  fragment catFields on Cat {
-    id
-  }
-`;
-
 const queryCats = `
   query {
-    listCats(limit: 2) {
-      items {
-        ...catFields
-      }
+    getRandom(limit: 2){
+      ...catFields
     }
   }
 
@@ -42,35 +26,16 @@ interface ICat {
 
 const CatsContainer = () => {
     const [started, setStarted] = useState(false);
-    const [ids, setIds] = useState([]);
-    const [id1, setId1] = useState('');
-    const [id2, setId2] = useState('');
     const [cats, setCats] = useState([]);
 
-    const randomIds = () => {
-      let randomId1 = ids[Math.floor(Math.random() * ids.length)];
-      let randomId2 = ids[Math.floor(Math.random() * ids.length)];
-    }
-
-    const fetchData = async () => {
-      const ids: any = await API.graphql(graphqlOperation(queryIds));
-      setIds(ids.data.listCats.items);
-      if (ids.length = 101) {
-        let randomId1 = ids[Math.floor(Math.random() * ids.length)];
-        let randomId2 = ids[Math.floor(Math.random() * ids.length)];
-        setId1(randomId1);
-        console.log(id1)
-        const data: any = await API.graphql(graphqlOperation(queryCats));
-        setCats(data.data.listCats.items);
-        return null
-      } else {
-        return alert("Refresh page to fetch cats")    
-      }
+    async function fetchCats() {
+      const data: any = await API.graphql(graphqlOperation(queryCats));
+      console.log(data.data);
     }
 
     const handleClick = () => {
       setStarted(true);
-      fetchData();
+      fetchCats();
     }
 
     const start = started === false
